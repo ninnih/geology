e048e1456ac5af0a260f61ae55a49f9dceb1fdfe
ninnih <contact@ninnihdesign.com>
2020-10-13 10:43:32 +0200

    ArcGis map added

--- client/src/modules/Map/Map.jsx
+++ client/src/modules/Map/Map.jsx
@@ -1,48 +1,25 @@
 import React, { useState, useEffect, useCallback } from 'react';
+import * as ReactDOM from 'react-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import './Map.scss';
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
-import { Polygon } from '@react-google-maps/api';
+import { loadCss } from 'esri-loader';
+import { Scene } from '@esri/react-arcgis';
+
+loadCss();
 
 const Map = () => {
-  const [map, setMap] = useState(null)
-  const containerStyle = {
-    width: '400px',
-    height: '90vh'
-  };
-   
-  const center = {
-    lat: 59.329323,
-    lng: 18.068581
-  };
-
-  const onLoad = useCallback(function callback(map) {
-    const bounds = new window.google.maps.LatLngBounds();
-    map.fitBounds(bounds);
-    setMap(map)
-  }, [])
- 
-  const onUnmount = useCallback(function callback(map) {
-    setMap(null)
-  }, [])
 
   return (
-    <main>
+    <main className="main--map">
       <section>
-      <LoadScript
-          googleMapsApiKey="AIzaSyCYFQFqOPNgSl24uqUzhTEs_qgJtLW48y4"
-        >
-          <GoogleMap
-            mapContainerStyle={containerStyle}
-            center={center}
-            zoom={10}
-            onLoad={onLoad}
-            onUnmount={onUnmount}
-          >
-            { /* Child components, such as markers, info windows, etc. */ }
-            <></>
-          </GoogleMap>
-        </LoadScript>
+        <Scene
+          style={{ width: '100vw', height: 'calc(100vh - 60px)' }}
+          mapProperties={{ basemap: 'satellite' }}
+          viewProperties={{
+              center: [59.329323, 18.068581],
+              zoom: 6
+          }}
+        />
       </section>
     </main>
   )
